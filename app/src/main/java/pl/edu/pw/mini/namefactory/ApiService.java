438cae00379e9c1d11bce6bf7c91ef74ee03e5e7
Adrian Sadłocha <adrian.adek@gmail.com>
2017-05-14 15:25:57 +0200

    API service: add `/user` endpoint

--- app/src/main/java/pl/edu/pw/mini/namefactory/ApiService.java
+++ app/src/main/java/pl/edu/pw/mini/namefactory/ApiService.java
@@ -1,13 +1,20 @@
 package pl.edu.pw.mini.namefactory;
 
+import java.util.HashMap;
+
 import retrofit2.Call;
 import retrofit2.Retrofit;
 import retrofit2.converter.gson.GsonConverterFactory;
+import retrofit2.http.Body;
 import retrofit2.http.GET;
+import retrofit2.http.POST;
 
 public interface ApiService {
     @GET("names_db")
     Call<ApiNamesDB> getNamesDB();
+
+    @POST("user")
+    Call<ApiNewUser> createNewUserAccount(@Body HashMap<String, String> body);
 
     Retrofit retrofit = new Retrofit.Builder()
             .baseUrl("https://api.namefactory.pl/")
