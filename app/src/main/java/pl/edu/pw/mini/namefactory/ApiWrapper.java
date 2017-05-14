36a1e756d1e0c60c1301521c99595efd63743de6
Adrian Sadłocha <adrian.adek@gmail.com>
2017-05-14 15:55:10 +0200

    Add API wrapper

---
+++ app/src/main/java/pl/edu/pw/mini/namefactory/ApiWrapper.java
@@ -0,0 +1,30 @@
+package pl.edu.pw.mini.namefactory;
+
+import java.io.IOException;
+
+public class ApiWrapper {
+    static private ApiService apiService = ApiService.retrofit.create(ApiService.class);
+    private String username;
+    private String password;
+
+    static ApiNewUser createNewUser(String email) throws IOException {
+        return apiService.createNewUserAccount(new ApiNewUserRequest(email)).execute().body();
+    }
+
+    static ApiNamesDB getNamesDB() throws IOException {
+        return apiService.getNamesDB().execute().body();
+    }
+
+    public ApiWrapper(String username, String password) {
+        this.username = username;
+        this.password = password;
+    }
+
+    public void createNewRanking(int ranking_id, int tag_ids[]) throws IOException {
+        apiService.createNewRanking(new ApiNewRankingRequest(this.username, this.password, ranking_id, tag_ids)).execute().body();
+    }
+
+    public void createNewMatch(int ranking_id, int winner_id, int loser_id) throws IOException {
+        apiService.createNewMatch(new ApiNewMatchRequest(this.username, this.password, ranking_id, winner_id, loser_id)).execute().body();
+    }
+}
