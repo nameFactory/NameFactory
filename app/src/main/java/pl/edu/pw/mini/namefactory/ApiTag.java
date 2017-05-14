d4ee3dd099bfee6a4f9233c8795f1a08b3d42bdc
Adrian Sadłocha <adrian.adek@gmail.com>
2017-05-14 14:41:04 +0200

    API service: add `/names_db` endpoint

---
+++ app/src/main/java/pl/edu/pw/mini/namefactory/ApiTag.java
@@ -0,0 +1,14 @@
+package pl.edu.pw.mini.namefactory;
+
+class ApiTag {
+    int id;
+    String name;
+
+    @Override
+    public String toString() {
+        return "ApiTag{" +
+                "id=" + id +
+                ", name='" + name + '\'' +
+                '}';
+    }
+}
---
+++ app/src/main/java/pl/edu/pw/mini/namefactory/NamesDB.java
@@ -0,0 +1,8 @@
+package pl.edu.pw.mini.namefactory;
+
+import java.util.List;
+
+class NamesDB {
+    List<ApiName> names;
+    List<ApiTag> tags;
+}
