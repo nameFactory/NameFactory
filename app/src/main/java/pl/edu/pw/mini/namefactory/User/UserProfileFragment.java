718600da8f4306e0f381fc3da73f606df77e094c
MartaM <marciszewiczm@gmail.com>
2017-05-16 22:24:17 +0200

    user profile

---
+++ app/src/main/java/pl/edu/pw/mini/namefactory/User/UserProfileFragment.java
@@ -0,0 +1,113 @@
+package pl.edu.pw.mini.namefactory.User;
+
+import android.content.Context;
+import android.net.Uri;
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.support.v7.app.AppCompatActivity;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import pl.edu.pw.mini.namefactory.R;
+
+/**
+ * A simple {@link Fragment} subclass.
+ * Activities that contain this fragment must implement the
+ * {@link UserProfileFragment.OnUserFragmentInteractionListener} interface
+ * to handle interaction events.
+ * Use the {@link UserProfileFragment#newInstance} factory method to
+ * create an instance of this fragment.
+ */
+public class UserProfileFragment extends Fragment {
+    // TODO: Rename parameter arguments, choose names that match
+    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
+    private static final String ARG_USERNAME = "username";
+    private static final String ARG_USEREMAIL = "email";
+
+    // TODO: Rename and change types of parameters
+    private UserAccount user = new UserAccount();
+
+    private OnUserFragmentInteractionListener mListener;
+
+    public UserProfileFragment() {
+        // Required empty public constructor
+    }
+
+    /**
+     * Use this factory method to create a new instance of
+     * this fragment using the provided parameters.
+     *
+
+     * @return A new instance of fragment UserProfileFragment.
+     */
+    // TODO: Rename and change types and number of parameters
+    public static UserProfileFragment newInstance(String name, String email) {
+        UserProfileFragment fragment = new UserProfileFragment();
+        Bundle args = new Bundle();
+        args.putString(ARG_USERNAME,name);
+        args.putString(ARG_USEREMAIL,email);
+        fragment.setArguments(args);
+        return fragment;
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        if (getArguments() != null) {
+
+            user.setEmail(getArguments().getString(ARG_USEREMAIL));
+            user.setUserName(getArguments().getString(ARG_USERNAME));
+            user.setName("MaciekUser");
+        }
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        View view = inflater.inflate(R.layout.fragment_user_profile, container, false);
+        ((AppCompatActivity) getActivity()).getSupportActionBar().setTitle(R.string.navigation_drawer_user_profile);
+        ((TextView)view.findViewById(R.id.userName)).setText(user.getName());
+
+        ((TextView)view.findViewById(R.id.showUserName)).setText(user.getUserName());
+        ((TextView)view.findViewById(R.id.showUserEmail)).setText(user.getEmail());
+
+
+        return view;
+    }
+
+
+    @Override
+    public void onAttach(Context context) {
+        super.onAttach(context);
+        if (context instanceof OnUserFragmentInteractionListener) {
+            mListener = (OnUserFragmentInteractionListener) context;
+        } else {
+            throw new RuntimeException(context.toString()
+                    + " must implement OnFragmentInteractionListener");
+        }
+    }
+
+    @Override
+    public void onDetach() {
+        super.onDetach();
+        mListener = null;
+    }
+
+    /**
+     * This interface must be implemented by activities that contain this
+     * fragment to allow an interaction in this fragment to be communicated
+     * to the activity and potentially other fragments contained in that
+     * activity.
+     * <p>
+     * See the Android Training lesson <a href=
+     * "http://developer.android.com/training/basics/fragments/communicating.html"
+     * >Communicating with Other Fragments</a> for more information.
+     */
+    public interface OnUserFragmentInteractionListener {
+        // TODO: Update argument type and name
+        void onUserFragmentInteraction(Uri uri);
+    }
+}
