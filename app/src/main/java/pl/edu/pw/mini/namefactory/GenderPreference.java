2f24df6badc19789677776fc33ba43d9b88daef9
MartaM <marciszewiczm@gmail.com>
2017-06-07 15:39:49 +0200

    przyciski boy girls2

--- app/src/main/java/pl/edu/pw/mini/namefactory/GenderPreference.java
+++ app/src/main/java/pl/edu/pw/mini/namefactory/GenderPreference.java
@@ -27,28 +27,37 @@ public class GenderPreference extends Preference {
     public void onBindViewHolder(PreferenceViewHolder holder) {
         super.onBindViewHolder(holder);
         holder.itemView.setClickable(false); // disable parent click
-        View bbutton = holder.findViewById(R.id.boyGender);
+        final View bbutton = holder.findViewById(R.id.boyGender);
+        final View gbutton = holder.findViewById(R.id.girldGender);
         bbutton.setClickable(true); // enable custom view click
         bbutton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 // persist your value here
-                SharedPreferences sharedPref = ((Activity)RankingsListMain.context).getPreferences(Context.MODE_PRIVATE);
+                SharedPreferences sharedPref = PreferenceManager.getDefaultSharedPreferences(RankingsListMain.context);
                 SharedPreferences.Editor editor = sharedPref.edit();
                 editor.putBoolean("gender", true);
                 editor.commit();
+                bbutton.setClickable(false);
+                gbutton.setClickable(true);
+                bbutton.setAlpha(.5f);
+                gbutton.setAlpha(1);
             }
         });
-        View gbutton = holder.findViewById(R.id.girldGender);
+
         gbutton.setClickable(true); // enable custom view click
         gbutton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 // persist your value here
-                SharedPreferences sharedPref = ((Activity)RankingsListMain.context).getPreferences(Context.MODE_PRIVATE);
+                SharedPreferences sharedPref = PreferenceManager.getDefaultSharedPreferences(RankingsListMain.context);
                 SharedPreferences.Editor editor = sharedPref.edit();
                 editor.putBoolean("gender", false);
                 editor.commit();
+                gbutton.setClickable(false);
+                bbutton.setClickable(true);
+                gbutton.setAlpha(.5f);
+                bbutton.setAlpha(1);
             }
         });
         // the rest of the click binding
